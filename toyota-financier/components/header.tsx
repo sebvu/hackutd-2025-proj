ff13bf0441fe26f52199e9047325a24c160be315
sebvu <santosjester738@gmail.com>
2025-11-08 17:40:16 -0600

    Simple re-styling

--- toyota-financier/components/header.tsx
+++ toyota-financier/components/header.tsx
@@ -2,18 +2,21 @@
 
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import "@/app/styles/header.css"; 
+import logo from "@/public/toyota_logo.png"
 
 const Header: React.FC = () => {
   return (
     <header className="header-container">
+      <div className="header-logo-container">
+        <Image className="header-logo" src={logo} alt="Toyota Logo"/>
+      </div>
       <h1>My Website</h1>
-
       <nav>
         <ul style={{ display: "flex", gap: "1rem", listStyle: "none" }}>
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/about">About</Link></li>
-          <li><Link href="/contact">Contact</Link></li>
+          <li><Link href="/">Favorites</Link></li>
+          <li><Link href="/about">Sign In</Link></li>
         </ul>
       </nav>
     </header>
